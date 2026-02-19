28bfbcc816c1def75cfafcec4d71f015241a8c24
Bahirul Rizki A <rizki.device@gmail.com>
2026-02-19 08:33:50 +0800

    feat: update prompt regex to match a more specific shell prompt and filter command and prompt lines from config output in the SonicNet model.

--- models/sonicnet.rb
+++ models/sonicnet.rb
@@ -2,10 +2,13 @@ class Sonicnet < Oxidized::Model
   using Refinements
 
   # match shell prompt
-  prompt /\w+>|#/
+  prompt /^[a-zA-Z0-9._-]+\@[a-zA-Z0-9._-]+\:(?:~|[\/a-zA-Z0-9._-]+)\s?[#$]\s*$/
 
   # read config from json file
-  cmd 'cat /etc/sonic/config_db.json'
+  cmd 'cat /etc/sonic/config_db.json' do |cfg|
+    cfg = cfg.lines[1..-2].join  # remove first (command) & last (prompt)
+    cfg
+  end
 
   cfg :ssh do
     pre_logout 'exit'
